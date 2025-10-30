674dd51f232b77601cf9fc3367da71710201f47d
hadifarooq4207-tech <hadifarooq4207@gmail.com>
2025-10-30 16:52:01 +0530

    Delete public directory

--- public/script.js
+++
@@ -1,9 +0,0 @@
-// Smooth button animations & basic navigation
-document.getElementById('getStartedBtn').addEventListener('click', () => {
-  alert('Sign up or log in to access your dashboard!');
-});
-
-// Placeholder login action
-document.getElementById('loginBtn').addEventListener('click', () => {
-  alert('Firebase login coming soon!');
-});
